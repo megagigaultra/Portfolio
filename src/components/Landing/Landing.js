d100d27630af80f630ccad53e86cedd87d5b2d6b
DIN <dindinmilou@gmail.com>
2021-01-25 22:54:26 +0100

    Setup Responsive
    Include-media : https://eduardoboucas.github.io/include-media/ + React-responsive : https://github.com/contra/react-responsive

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 import landingImage from '../../assets/img/fly.jpg'
 import { ReactComponent as Arrow } from '../../assets/svg/callarrow.svg'
 
-
 function Landing() {
+    const isTablet = useMediaQuery({
+        query: '(min-device-width: 500px)'
+    })
     return (
         <>
             <div className="landing">
                 <div className="background index1">
-                    <div className="container shape index3">
-                        <div className="circle"></div>
-                    </div>
+                    {
+                        !isTablet &&
+                            <div className="container shape index3">
+                                <div className="circle"></div>
+                            </div>
+                    }
                     <div className="landing-img">
                         {/* <span className="scroll index4">Scroll</span> */}
                         <div className="gradient index2"></div>
@@ -18,14 +24,11 @@ function Landing() {
                         <img src={landingImage} alt="background letters"/>
                     </div>
                 </div>
-                <div className="container calltoaction">
+                <div className="container footy index1">
                     {/* <span className="asterisk">*Under construction</span> */}
                     <h2 className="bgc">Stay in</h2>
                     <h2>Touch</h2>
-                </div>
-                <div className="container footy">
-                        <span className="asterisk">* Under construction</span>
-                        <Arrow className="arrow" />
+                    <Arrow className="arrow" />
                 </div>
             </div>
         </>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,31 +11,23 @@ function Navbar() {
     return (
         <>
                 <div className="navbar">
-                    <div className="container">
-                        <div className="navbar-logo">
-                            <Link to='/' className="nav-links">
-                                <Logo className="navbar-icon" />
-                            </Link>
+                    <div className="media">
+                        <div className="container">
+                            <div className="navbar-logo">
+                                <Link to='/' className="nav-links">
+                                    <Logo className="navbar-icon" />
+                                </Link>
+                            </div>
+                            <IconContext.Provider value={{ color: '#fff'}}>
+                                <ul className="navbar-social">
+                                    <li className="nav-item last">
+                                        <Link to='/' className="nav-links">
+                                            <RiSearch2Line className="search-icon" />
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </IconContext.Provider>
                         </div>
-                        <IconContext.Provider value={{ color: '#fff'}}>
-                            <ul className="navbar-social">
-                                {/* <li className="nav-item first">
-                                    <Link to='/' className="nav-links">
-                                        <SiTwitter className="navbar-icon" />
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to='/' className="nav-links">
-                                        <FaGithub className="navbar-icon" />
-                                    </Link>
-                                </li> */}
-                                <li className="nav-item last">
-                                    <Link to='/' className="nav-links">
-                                        <RiSearch2Line className="search-icon" />
-                                    </Link>
-                                </li>
-                            </ul>
-                        </IconContext.Provider>
                     </div>
                 </div>
             
